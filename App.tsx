f17beb74150941c8060ff4cb412ba0ddb9d29f33
Viktor Barta <viktor.barta@techzia.hu>
2024-10-17 12:20:00 +0200

    base

--- App.tsx
+++ App.tsx
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Pressable,
+  Alert,
+} from 'react-native';
+import { theme } from './theme';
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      'Are you sure you want to delete this?',
+      'it will be gone for good?',
+      [
+        {
+          text: 'Yes',
+          onPress: () => console.log('Oke deleting.'),
+          style: 'destructive',
+        },
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+      ]
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.itemContainer}>
+        <Text style={{ fontSize: 18, fontWeight: 500 }}>Coffee</Text>
+
+        <TouchableOpacity
+          onPress={handleDelete}
+          activeOpacity={0.7}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +51,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  itemContainer: {
+    borderBottomColor: 'red',
+    borderBottomWidth: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
   },
 });
---
+++ theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  colorMain: '#404E5C',
+  colorWhite: '#fff',
+  colorBlack: '#000',
+};
